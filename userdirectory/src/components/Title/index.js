5d3d1f15e497a4dca16f9661a889b71ce37eac0d
Guillermo Garcia <ggq0107@hotmail.com>
2021-05-06 19:36:02 -0400

    initial push, main skeleton

---
+++ userdirectory/src/components/Title/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Title(props) {
+  return <h1 className="title">Employee List</h1>;
+}
+
+export default Title;
