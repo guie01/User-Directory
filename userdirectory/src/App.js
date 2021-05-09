de1eeb088738bf800268123ca558d4b100b57a80
Guillermo Garcia <ggq0107@hotmail.com>
2021-05-09 12:41:03 -0400

    get request from API, fixed table to show content from API and sorting by first and last anme

--- userdirectory/src/App.js
+++ userdirectory/src/App.js
@@ -2,39 +2,61 @@ import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-
+import API from "./utils/API";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.sortByFirstName = this.sortByFirstName.bind(this)
+  }
   // Setting this.state.friends to the friends json array
   state = {
-    // friends
+    users: []
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
+
+  componentDidMount(){
+    API.getUsers().then((results) => {
+      console.log(results);
+
+      const userList = results.data.results;
+
+      this.setState({
+        users: userList
+      })
+    });
+  }
+
+  sortByFirstName() {
+
+    const userList = this.state.users
+
+    userList.sort(function(user1, user2) {
+      if (user1.name.first < user2.name.first) {
+        return -1;
+      }
+      if (user1.name.first > user2.name.first) {
+        return 1;
+      }
+    
+      // names must be equal
+      return 0;
+      });
+
+      this.setState({
+        users: userList
+      })
+
+  }
+
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
         <Title></Title>
-        {/* {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))} */}
-
-      <FriendCard/>
+        
+      <FriendCard users = {this.state.users} sortFirst = {this.sortByFirstName}/>
       </Wrapper>
     );
   }
--- userdirectory/src/components/FriendCard/index.js
+++ userdirectory/src/components/FriendCard/index.js
@@ -6,30 +6,21 @@ function FriendCard(props) {
     <table class="table table-striped">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Picture</th>
+      <th scope="col">ID</th>
+      <th scope="col">First Name  <button onClick={props.sortFirst}>Sort</button></th>
+      <th scope="col">Last Name</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+    {props.users.map((user, index) => (
+          <tr>
+          <td><img src = {user.picture.thumbnail}></img></td>
+          <th scope="row">{index + 1}</th>
+          <td>{user.name.first}</td>
+          <td>{user.name.last}</td>
+        </tr>
+        ))}
   </tbody>
 </table>
   );
